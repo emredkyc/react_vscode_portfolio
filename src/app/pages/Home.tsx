6b1e71350f9c9707960b7077e970285708865c71
Emre Dokuyucu <emredkyc@hotmail.com>
2026-01-06 10:33:12 +0100

    update home page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -52,7 +52,7 @@ export default function Home({ setSelectedIndex }: Props) {
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
               <Typography variant="subtitle1" gutterBottom>
-              Enthusiastic Software Developer 
+              Senior Software Engineer 
               </Typography>
             </Grid>
             <Grid
